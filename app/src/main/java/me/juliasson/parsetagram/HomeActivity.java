65e6e9988e371661bfded782fa0caa7944f2d97d
Julia Son <juliasson@fb.com>
2018-07-10 14:33:48 -0700

    camera runs with button click

--- app/src/main/java/me/juliasson/parsetagram/HomeActivity.java
+++ app/src/main/java/me/juliasson/parsetagram/HomeActivity.java
@@ -1,13 +1,22 @@
 package me.juliasson.parsetagram;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -26,6 +35,20 @@ public class HomeActivity extends AppCompatActivity {
     private Button bvCreate;
     private Button bvRefresh;
     private Button bvSettings;
+    private ImageView ivPreview;
+
+    private Button button2;
+
+    public final String APP_TAG = "home_activity";
+    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
+    public String photoFileName = "photo.jpg";
+    File photoFile;
+
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
+
+    //TODO: replace file path with camera stuff
+    private final String FILE_PATH = "/storage/emulated/0/DCIM/Restored/20180704_102825.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +62,15 @@ public class HomeActivity extends AppCompatActivity {
         bvCreate = findViewById(R.id.bvCreate);
         bvRefresh = findViewById(R.id.bvRefresh);
         bvSettings = findViewById(R.id.bvSettings);
+        ivPreview = findViewById(R.id.ivPreview);
+        button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onLaunchCamera(view);
+            }
+        });
 
         bvCreate.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,7 +78,7 @@ public class HomeActivity extends AppCompatActivity {
                 final String description = etDescription.getText().toString();
                 final ParseUser user = ParseUser.getCurrentUser();
 
-                final File image = new File("/storage/emulated/0/DCIM/Restored/20180704_102825.jpg");
+                final File image = new File(FILE_PATH);
                 final ParseFile parseFile = new ParseFile(image);
 
                 parseFile.saveInBackground(new SaveCallback() {
@@ -117,5 +149,60 @@ public class HomeActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    //----------------CAMERA-------------------
+    public void onLaunchCamera(View view) {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference to access to future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "me.juliasson.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(APP_TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
+                ivPreview.setImageBitmap(takenImage);
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/me/juliasson/parsetagram/MainActivity.java
+++ app/src/main/java/me/juliasson/parsetagram/MainActivity.java
@@ -3,7 +3,6 @@ package me.juliasson.parsetagram;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -25,9 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.tbToolBar);
-        setSupportActionBar(toolbar);
 
         ParseUser currentUser = ParseUser.getCurrentUser();
         if (currentUser != null) {
