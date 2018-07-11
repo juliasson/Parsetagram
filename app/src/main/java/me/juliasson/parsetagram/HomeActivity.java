67a4f5951ee359b65d948825246f241c5a8586f0
Julia Son <juliasson@fb.com>
2018-07-10 17:31:23 -0700

    functioning recyclerview and toolbar update

--- app/src/main/java/me/juliasson/parsetagram/HomeActivity.java
+++ app/src/main/java/me/juliasson/parsetagram/HomeActivity.java
@@ -1,138 +1,67 @@
 package me.juliasson.parsetagram;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import android.support.annotation.Nullable;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.view.Menu;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
-import com.parse.ParseFile;
-import com.parse.ParseUser;
-import com.parse.SaveCallback;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import me.juliasson.parsetagram.model.Post;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private EditText etDescription;
-    private Button bvCreate;
-    private Button bvRefresh;
-    private Button bvSettings;
-    private ImageView ivPreview;
-
-    private Button button2;
-
-    public final String APP_TAG = "home_activity";
-    public final static int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034;
-    public String photoFileName = "photo.jpg";
-    File photoFile;
-
-    private ShareActionProvider miShareAction;
-    private Intent shareIntent;
-
-    //TODO: replace file path with camera stuff
-    private final String FILE_PATH = "/storage/emulated/0/DCIM/Restored/20180704_102825.jpg";
+    private SwipeRefreshLayout swipeContainer;
+    PostAdapter postAdapter;
+    ArrayList<Post> posts;
+    RecyclerView rvPosts;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
         Toolbar toolbar = findViewById(R.id.tbToolBar);
         setSupportActionBar(toolbar);
 
-        etDescription = findViewById(R.id.etDescription);
-        bvCreate = findViewById(R.id.bvCreate);
-        bvRefresh = findViewById(R.id.bvRefresh);
-        bvSettings = findViewById(R.id.bvSettings);
-        ivPreview = findViewById(R.id.ivPreview);
-        button2 = findViewById(R.id.button2);
-
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onLaunchCamera(view);
-            }
-        });
-
-        bvCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String description = etDescription.getText().toString();
-                final ParseUser user = ParseUser.getCurrentUser();
-
-                final File image = getPhotoFileUri(photoFileName);
-                final ParseFile parseFile = new ParseFile(image);
-
-                parseFile.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if (e == null) {
-                            createPost(description, parseFile, user);
-                        } else {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-
-            }
-        });
-
-        bvRefresh.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadTopPosts();
-            }
-        });
-
-        bvSettings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i = new Intent(HomeActivity.this, SettingsActivity.class);
-                startActivity(i);
-            }
-        });
+        rvPosts = findViewById(R.id.rvPosts);
+        posts = new ArrayList<>();
+        postAdapter = new PostAdapter(posts);
+        rvPosts.setLayoutManager(new LinearLayoutManager(this));
+        rvPosts.setAdapter(postAdapter);
 
         loadTopPosts();
-    }
 
-    private void createPost(String description, ParseFile image, ParseUser user) {
-        final Post newPost = new Post();
-        newPost.setDescription(description);
-        newPost.setImage(image);
-        newPost.setUser(user);
-
-        newPost.saveInBackground(new SaveCallback() {
+        //find the swipe container
+        swipeContainer = findViewById(R.id.swipeContainer);
+        // Setup refresh listener which triggers new data loading
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
-            public void done(ParseException e) {
-                if (e == null) {
-                    Log.d("HomeActivity", "Create post success");
-                    Toast.makeText(HomeActivity.this, "Post created!", Toast.LENGTH_SHORT).show();
-                } else {
-                    e.printStackTrace();
-                }
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                refresh();
             }
         });
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void loadTopPosts() {
@@ -144,9 +73,12 @@ public class HomeActivity extends AppCompatActivity {
             public void done(List<Post> objects, ParseException e) {
                 if (e == null) {
                     for(int i = 0; i < objects.size(); i++) {
-                        Log.d("HomeActivity", "Post[" + i + "] = "
+                        Log.d("CreatePostActivity", "Post[" + i + "] = "
                                 + objects.get(i).getDescription()
                                 + "\nusername: " + objects.get(i).getUser().getUsername());
+                        Post post = objects.get(i);
+                        posts.add(post);
+                        postAdapter.notifyItemInserted(posts.size() - 1);
                     }
                 } else {
                     e.printStackTrace();
@@ -155,75 +87,11 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    //----------------CAMERA-------------------
-    public void onLaunchCamera(View view) {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference to access to future access
-        photoFile = getPhotoFileUri(photoFileName);
-
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "me.juliasson.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-    // Returns the File for a photo stored on disk given the fileName
-    public File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(APP_TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
-        return file;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                // RESIZE BITMAP, see section below
-                Bitmap resizedBitmap = BitmapScaler.scaleToFitWidth(takenImage, 600);
-                // Configure byte output stream
-                ByteArrayOutputStream bytes = new ByteArrayOutputStream();
-                // Compress the image further
-                resizedBitmap.compress(Bitmap.CompressFormat.JPEG, 40, bytes);
-                // Create a new file for the resized bitmap (`getPhotoFileUri` defined above)
-                Uri resizedUri = Uri.fromFile(getPhotoFileUri(photoFileName + "_resized"));
-                File resizedFile = new File(resizedUri.getPath());
-                try {
-                    resizedFile.createNewFile();
-                    FileOutputStream fos = new FileOutputStream(resizedFile);
-                    // Write the bytes of the bitmap to file
-                    fos.write(bytes.toByteArray());
-                    fos.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // Load the taken image into a preview
-                ImageView ivPreview = (ImageView) findViewById(R.id.ivPreview);
-                ivPreview.setImageBitmap(resizedBitmap);
-            } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        }
+    public void refresh() {
+        postAdapter.clear();
+        loadTopPosts();
+        postAdapter.addAll(posts);
+        // Now we call setRefreshing(false) to signal refresh has finished
+        swipeContainer.setRefreshing(false);
     }
 }
---
+++ app/src/main/java/me/juliasson/parsetagram/PostAdapter.java
@@ -0,0 +1,99 @@
+package me.juliasson.parsetagram;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+import me.juliasson.parsetagram.model.Post;
+
+public class PostAdapter extends RecyclerView.Adapter<PostAdapter.ViewHolder> {
+
+    private List<Post> mPosts;
+    private Context mContext;
+
+    public PostAdapter(List<Post> posts) {
+        mPosts = posts;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        mContext = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+
+        View postView = inflater.inflate(R.layout.item_post, parent, false);
+        ViewHolder viewHolder = new ViewHolder(postView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PostAdapter.ViewHolder holder, int position) {
+        Post post = mPosts.get(position);
+        holder.tvUserName.setText(post.getUser().getUsername());
+        holder.tvDescription.setText(post.getDescription());
+
+        Glide.with(mContext)
+                .load(post.getImage().getUrl())
+                .into(holder.ivImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPosts.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        public ImageView ivProfileImage;
+        public TextView tvUserName;
+        public ImageButton ibOptions;
+        public ImageView ivImage;
+        public ImageButton ibLike;
+        public ImageButton ibComment;
+        public ImageButton ibMessage;
+        public ImageButton ibBookmark;
+        public TextView tvDescription;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            ivProfileImage = itemView.findViewById(R.id.ivProfileImage);
+            tvUserName = itemView.findViewById(R.id.tvUserName);
+            ibOptions = itemView.findViewById(R.id.ibOptions);
+            ivImage = itemView.findViewById(R.id.ivImage);
+            ibLike = itemView.findViewById(R.id.ibLike);
+            ibComment = itemView.findViewById(R.id.ibComment);
+            ibMessage = itemView.findViewById(R.id.ibMessage);
+            ibBookmark = itemView.findViewById(R.id.ibBookmark);
+            tvDescription = itemView.findViewById(R.id.tvDescription);
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+
+    // Clean all elements of the recycler
+    public void clear() {
+        mPosts.clear();
+        notifyDataSetChanged();
+    }
+
+    // Add a list of items -- change to type used
+    public void addAll(List<Post> list) {
+        mPosts.addAll(list);
+        notifyDataSetChanged();
+    }
+}
